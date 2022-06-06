c8f05d0a3ab0ccc61fe331f62218ba30c291d847
fkilld <fkillazad@gmail.com>
2022-06-07 01:36:30 +0530

    menu

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import Menu from './Menu'
+import Categories from './Categories'
+import items from './data'
+
+// this way we didn't get repeat
+const allCategories = ['all', ...new Set(items.map((item) => item.category))]
+console.log(allCategories)
 
 function App() {
+  const [menuItems, setMenuItems] = useState(items)
+  const [categories, setCategories] = useState(allCategories)
+
+  const filterItems = (category) => {
+    if (category === 'all') {
+      setMenuItems(items)
+      return
+    }
+    const newItems = items.filter((item) => item.category === category)
+    setMenuItems(newItems)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main>
+      <section className='menu section'>
+        <div className='title'>
+          <h2>our menu</h2>
+          <div className='underline'></div>
+        </div>
+        <Categories filterItems={filterItems} categories={categories} />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  )
 }
 
-export default App;
+export default App
---
+++ src/Categories.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Categories = ({ categories, filterItems }) => {
+  return (
+    <div className='btn-container'>
+      {categories.map((category, index) => {
+        return (
+          <button
+            className='filter-btn'
+            type='button'
+            key={index}
+            onClick={() => filterItems(category)}
+          >
+            {category}
+          </button>
+        )
+      })}
+    </div>
+  )
+}
+
+export default Categories
